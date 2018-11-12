6306c195b156f733e8657351229b27ed377d6167
Jen Carrigan <jen.d.carrigan@gmail.com>
2018-11-12 13:02:01 -0800

    adding more schemas

--- src/models/stops.js
+++ src/models/stops.js
@@ -1,11 +1,11 @@
 //--------------------------------------
-//* Route Stops Schema
+//* Stops Schema
 //--------------------------------------
 
 import mongoose from 'mongoose';
 
 const stopsSchema = new mongoose.Schema({
-  location: { type: String, required: true, unique: true },
+  route: { type: Schema.Types.ObjectId, ref: 'route' },
 });
 
 export default mongoose.model('stop', stopsSchema);
