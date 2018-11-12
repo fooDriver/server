54b9bc5f1dc0ecf50dd6a749b7813a327552f397
Jen Carrigan <jen.d.carrigan@gmail.com>
2018-11-12 11:17:57 -0800

    initial userSchema

---
+++ src/models/user-donator.js
@@ -0,0 +1,22 @@
+//--------------------------------------
+//* User Schema
+//--------------------------------------
+
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true, default: 'user', enum: ['admin', 'driver', 'donator', 'user'] },
+});
+
+const capabilities = {
+  user: ['read'],
+  donator: ['read'],
+  driver: ['read, update, delete'],
+  admin: ['create', 'read', 'update', 'delete'],
+};
+
+export default mongoose.model('users', userSchema);
