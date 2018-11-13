38c8b88b327a06d23fe0075d704cebcc2393cbc9
Ben Harris <ben@harrishills.com>
2018-11-13 09:18:17 -0800

    Removing CLI code from repository

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@
 
 // Dependencies
 import express from 'express';
-import cli from './views/cli.js';
 // import router from './api/router.js';
 // import notFound from './middleware/404.js'; // 404 page not found
 // import serverError from './middleware/error.js'; // 500 server error
@@ -32,18 +31,18 @@ module.exports = {
       app.listen(port, (err) => {
         if(err) { throw err; }
         isRunning = true;
-        console.log('Connected to WEB server on port:', port); // TODO: remove once CLI
+        console.log('Connected to WEB server on port:', port);
       });
     }
     else {
-      console.log('Server is already running'); // TODO: remove once CLI installed
+      console.log('Server is already running');
     }
   },
 
   stop: () => {
     app.close( () => {
       isRunning = false;
-      console.log('Server has been stopped'); // TODO: remove once CLI installed
+      console.log('Server has been stopped');
     });
   },
 };
