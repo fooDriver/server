12c81dac0ee87694b37484dde5ebf679c43bfa69
Jane Smith <example@email.com>
2018-11-14 09:37:04 -0800

    revised driver and user test

--- __tests__/user-router.test.js
+++ __tests__/user-router.test.js
@@ -1,15 +1,7 @@
-// import mongoose from 'mongoose';
-// import supertest from 'supertest';
-// import User from '../src/models/users';
-// import auth from '../src/middleware/auth';
-// import auth from '../src/middleware/404';
-// import auth from '../src/middleware/error';
-// import auth from '../src/middleware/sendJSON';
 
-// describe('User router', () => {
-//   it('throws an error if no authorization header present', () => {});
-//   it('returns an error when username and password are both not present', () => {});
-//   it('returns an error when the username is not present', () => {});
-//   it('returns an error when the password is not present', () => {});
-//   it('passes on a token when a username and password are present', (done) => {});
-// });
+
+describe('User router', () => {
+  it('should get all the driver routs', () => {});
+  it('should get driver rout with driver name', () => {});
+  it('should post a request with driver name', () => {});
+});
