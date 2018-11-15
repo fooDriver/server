ef078be9d2ea27ac32ef272905012590999a44c8
Jane Smith <example@email.com>
2018-11-14 16:11:31 -0800

    still working on driver rout test

--- src/api/driver-router.js
+++ src/api/driver-router.js
@@ -20,7 +20,6 @@ import sendJSON from '../middleware/sendJSON';
 // routes
 
 driverRouter.get('/driver-routes/:name', auth('driver'), async (req, res, next) => {
-  console.log(req.params, "IM HEREEEEEEEE");
   let driver = await users.findOne({username: req.params.name});
   res.send(driver);
 });
@@ -28,9 +27,10 @@ driverRouter.get('/driver-routes/:name', auth('driver'), async (req, res, next) 
 
 driverRouter.post('/driver-routes/:name', auth('driver'), async (req, res, next) => {
   let driver = await users.findOne({username: req.params.name});
-  driver.pantry.push(req.body.id);
-  await driver.save();
-  res.send(driver);
+  let driverPantry = await pantry.findOne({driver: driver._id});
+  driverPantry.pantryItems.push(req.body._id);
+  await driverPantry.save();
+  res.send(driverPantry);
 });
 
 // driverRouter.put('/driver-routes/:name', (req, res, next) => {
@@ -41,12 +41,13 @@ driverRouter.post('/driver-routes/:name', auth('driver'), async (req, res, next)
 driverRouter.delete('/driver-routes/:name/:foodid', auth('driver'), async(req, res, next) => {
 
   let driver = await users.findOne({username: req.params.name});
-  const index = driver.pantry.findIndex(food => JSON.stringify(food._id) == JSON.stringify(req.params.foodid));
+  let driverPantry = await pantry.findOne({driver: driver._id});
+  const index = driverPantry.pantryItems.findIndex(food => JSON.stringify(food._id) == JSON.stringify(req.params.foodid));
   if (!index) {
     return 'Does not exist!';
   }
-  driver.pantry.splice(index,1);
-  await driver.save();
+  driverPantry.pantryItems.splice(index,1);
+  await driverPantry.save();
 
   res.statusCode = 204;
   res.send('That food was deleted');
